ceb4caea1fa543209aa4bf611651f1f785ec7e71
Chirinoxi <32821226+Chirinoxi@users.noreply.github.com>
2017-11-04 23:41:03 -0300

    Update NodoMatrix.cpp

--- Taller2/NodoMatrix.cpp
+++ Taller2/NodoMatrix.cpp
@@ -2,14 +2,16 @@
 #include "NodoMatrix.h"
 
 
-NodoMatrix::NodoMatrix()
-{
+NodoMatrix::NodoMatrix() {
+
 }
 
-NodoMatrix::NodoMatrix(bool b, Triple * t)
-{
+NodoMatrix::NodoMatrix(bool b, Triple* t) {
+
 	this->head = b;
+
 	if (b) {
+
 		this->left = this;
 		this->up = this;
 		this->triple = *t;
@@ -19,13 +21,13 @@ NodoMatrix::NodoMatrix(bool b, Triple * t)
 	}
 }
 
-NodoMatrix * NodoMatrix::getLeft()
-{
-	return this->getLeft;
+NodoMatrix * NodoMatrix::getLeft() {
+
+	return this->left;
 }
 
-Triple NodoMatrix::getTriple()
-{
+Triple NodoMatrix::getTriple() {
+
 	return this->triple;
 }
 
@@ -34,45 +36,48 @@ NodoMatrix * NodoMatrix::getUp()
 	return this->up;
 }
 
-int NodoMatrix::getCol()
-{
+int NodoMatrix::getCol() {
+
 	return this->triple.col;
 }
 
-int NodoMatrix::getRow()
-{
+int NodoMatrix::getRow() {
+
 	return this->triple.row;
 }
 
-string NodoMatrix::getValue()
-{
+string NodoMatrix::getValue() {
+
 	return this->triple.value;
 }
 
-void NodoMatrix::setUp(NodoMatrix * down)
-{
+void NodoMatrix::setUp(NodoMatrix * down) {
+
 	this->up = down;
 }
 
-void NodoMatrix::setLeft(NodoMatrix * right)
-{
+void NodoMatrix::setLeft(NodoMatrix * right) {
+
 	this->left = right;
 }
 
-NodoMatrix & NodoMatrix::operator=(const NodoMatrix & nodo)
-{
-	// TODO: insertar una instrucción return aquí
+NodoMatrix & NodoMatrix::operator=(const NodoMatrix & nodo) {
+
 	this->head = nodo.head;
 	this->left = nodo.left;
 	this->up = nodo.up;
 	this->triple = nodo.triple;
 	return *this;
+
 }
 
+NodoMatrix::~NodoMatrix() {
 
-NodoMatrix::~NodoMatrix()
-{
 	delete up;
 	delete left;
 
+
 }
+
+
+
